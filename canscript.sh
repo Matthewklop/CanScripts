b4da44565f35bb0b658e3faff7f480f34b00e31a
Matthewklop <60280846+Matthewklop@users.noreply.github.com>
2020-04-14 00:11:25 -0400

    User friendly

--- canscript.sh
+++ canscript.sh
@@ -1,17 +1,18 @@
-#run this file by typing "sh canscript.sh"
-
 a=10
 b=10
-funcOn="vcan0 100 "
+CanID=100
+CanData="  99 99" #can to seatch with spaces | also double space at start "optional"
+CanToSend="vcan0 100#0000000000000000"
+
 while [ $a = $b ] 
 do
 #after grep put ID
-  st1=$(timeout 0.1s candump vcan0 | grep  "100" | cut -d "]" -f2)	
+  st1=$(timeout 0.1s candump vcan0 | grep  "$CanID" | cut -d "]" -f2)	
 printf "$st1"
 # after = put data in quote
-if [ "$st1" = "  99 99" ]; then
-echo string found
-cansend vcan0 100#0000000000000000
+if [ "$st1" = "$CanData" ]; then
+echo " !string found! "
+cansend $CanToSend
 fi
 st=""
 done
